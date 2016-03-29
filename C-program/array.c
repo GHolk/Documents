a36254f1434c9091edc580f67a5da8688ea5d520
gholk <c34031328@mail.ncku.edu.tw>
2016-03-29 21:55:31 +0800

    [new] finish survey 11 asignment
    consider using flickr as picture base.
    svg by inkscape suck.

---
+++ C-program/array.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main( int argc, char *argv[] ){
+
+for( i = 0 ; i < argc ; i++ ) printf( "arg %d = %s\n", i, argv[i]); 
+
+char *lang[] = { "C", "C++", "Objective-C", "Java", "C#" }; 
+
+for( i = 0 ; i < 5 ; i++ ) printf( "lang %d = %s\n", i, lang[i] );
+
+return 0; 
+}
