457c4d017b80adcbc3232b2a1eb8675ca0c59583
gholk <c34031328@mail.ncku.edu.tw>
2016-04-04 17:10:15 +0800

    [add] least square hw2 finish 8 chapter
    start C program HW1.c

--- C-program/HW1_C34031328.c
+++ C-program/HW1_C34031328.c
@@ -1,30 +1,96 @@
 
 #include <stdio.h>
 
+int add( int a, int b, int c ){ return a+b+c ; }
+
+
+int times( int a, int b, int c ){ return a*b*c ; }
+
+
+
+
+int maximan( int a , int b , int c ){ 
+
+int max = a ; 
+
+if( max > b ) max = b ; 
+if( max > c ) max = c ; 
+
+return max ; 
+}
+
+
+
+int minimal( int a , int b , int c ){
+
+if( a < b ) a = b ; 
+if( a < c ) a = c ; 
+
+return a ; 
+}
+
+
+
+float average( int a, int b, int c ) { 
+	return ( (float) a + (float) b + (float) c )/3 ; 
+	}
+
+
+float var( int a, int b, int c ){
+
+float mean, variance, fa, fb, fc ;  
+
+mean = average( a, b, c ) ; 
+
+fa = (float) a - mean ; 
+fb = (float) b - mean ; 
+fc = (float) c - mean ; 
+
+fa *= fa ; 
+fb *= fb ; 
+fc *= fc ; 
+
+variance = ( fa + fb + fc )/3 ; 
+
+return variance ; 
+
+}
+
+
+
+
 int main( int argc , char *argv[] ){
 
+/*
 if( argc != 3 ){
-
 	puts("you can only input 3 number! \n"); 
 	return 3;
 	}
+*/
+
+int a = 10 , b = 20 , c = 30 ; 
+
+int sum, prod, min, max ; 
+float mean ; 
+float variance ; 
+
+sum = add( a, b, c ); 
+
+prod = times( a, b, c ); 
+
+mean = average( a, b, c ); 
+
+variance = var( a, b, c ); 
+
+max = maximan( a, b, c ); 
+
+min = minimal( a, b, c );
 
 
+printf("sum = %d ; \nprod = %d ; \nmean = %f ; \nvar = %f ; \n", 
+	sum, prod, mean, variance ); 
 
-int i = 0 ; 
-
-double sum = 0 , prod = 1 , mean = 0 , var , max, min, mid ; 
-
-for( i = 1 ; i < argc ; i++ ){
-
-	sum += (double) argv[i] ; 
-	prod *= (double) argv[i] ; 
-	
-	}
-
-printf( "sum = %f ; \nprod = %f ; \n", 
-	sum, prod 
-	);
+printf("max = %d ; \nmin = %d ; \n", max, min ); 
 
 return 0 ;
 
