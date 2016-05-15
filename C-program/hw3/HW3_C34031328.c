19c4b9f56d14fc910b4c17effb6b07035ad0ac8e
gholk <c34031328@mail.ncku.edu.tw>
2016-05-15 23:55:24 +0800

    [new] survey homework 13

--- C-program/hw3/HW3_C34031328.c
+++ C-program/hw3/HW3_C34031328.c
@@ -1,6 +1,8 @@
 /* 程式設計 - 作業三 */
 #include <stdio.h>
 #include <stdlib.h>
+# define SWAP(X,Y)  do { X=X^Y; Y=X^Y; X=X^Y; } while (0)
+
 /* A.回傳N與M的大小關係（>或<或=） */
 char relation(int N, int M)
 {
@@ -18,37 +20,50 @@ int distance(int N, int M)
 /* C.回傳N到M之間（含N與M）所有偶數之個數 */
 int evenCount(int N, int M)
 {
-	int L=N-M ;
-	if (L%2 || !N%2) return L/2+1; 
+	int L=distance(N,M); 
+	if ( (L%2) || !(N%2) ) return L/2+1; 
 	else return L/2; 
 }
 
 /* D.回傳N到M之間（含N與M）所有奇數之總和 */
 int oddSum(int N, int M)
 {
-    //請完成此函數 
-    return 0;
+	int L=distance(N,M); 
+	if ( (L%2) || (N%2) ) return L/2+1; 
+	else return L/2; 
 }
 
 /* E.回傳N與M共計幾位數 */
 int digit(int N, int M)
 {
-    //請完成此函數 
-    return 0;
+	int i=0; 
+	for ( ; N>0; i++) N/=10;
+	for ( ; M>0; i++) M/=10;
+	return i;
 }
 
 /* F.回傳N與M之間有幾個平方數 */
 int squareCount(int N, int M)
 {
-    //請完成此函數 
-    return 0;
+	int i=0, j=0; 
+	if (N>M) SWAP(N,M); 
+	for (i=0; i*i<=M; i++) if (i*i>=N) j++;
+	return j;
 }
 
 /* G.回傳N與M中有幾個一樣的數字 */
 int same(int N, int M)
 {
-    //請完成此函數 
-    return 0;
+
+	//short int num[10]=0 ;
+	char Ns[10], Ms[10]; 
+	sprintf(Ns,"%d",N);
+	sprintf(Ms,"%d",M);
+	for (int i=0; Ns[i]!='\0'; i++)
+		putchar( Ns[i] );
+
+	return 0;
+	
 }
 
 /* H.回傳N點M分時，時針與分針的夾角 */
@@ -70,7 +85,7 @@ void parallelogram(int N, int M)
     //請完成此函數 
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	int N, M;
 	
@@ -82,11 +97,11 @@ int main(void)
 	printf("B.距離: %d\n", distance(N, M));
 	*/
 	printf("C.偶數個數: %d\n", evenCount(N, M));
-	/*
 	printf("D.奇數總和: %d\n", oddSum(N, M));
 	printf("E.共計幾位數: %d\n", digit(N, M));
 	printf("F.平方數個數: %d\n", squareCount(N, M));
 	printf("G.一樣的數字: %f\n", angle(N, M));
+	/*
 	printf("H.時鐘夾角: %d\n", carry(N, M));
 	printf("I.NxM長方形:\n");
 	rectangle(N, M);
