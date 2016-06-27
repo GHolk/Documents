3b32ff67e96a05a5f105d097dee3f7101e246f73
gholk <c34031328@mail.ncku.edu.tw>
2016-06-27 12:20:43 +0800

    [new] a new branch after semester end

--- C-program/remo-cli/online2.c
+++
@@ -1,7 +0,0 @@
-
-#include<stdio.h>
-int main(){
-for( i=0; i<3; i++)
-puts("Hello World!");
-return 0;
-}
--- C-program/remo-cli/online3.c
+++
@@ -1,9 +0,0 @@
-
-#include<stdio.h>
-int main(){
-int i, n;
-scanf("%7d",&n);
-for( i=0; i<n; i++)
-puts("Hello World!");
-return 0;
-}
